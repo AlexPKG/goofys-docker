879d46a9212d193e8fdf43db6fc7996293b12f90
Alex P <alex.pekurovsky@kindgeek.com>
2022-10-26 14:42:11 +0300

    optional aws access keys

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 mkdir -p ~/.aws
-cat<<EOF>~/.aws/credentials
+if [[ ! -z "${AWS_ACCESS_KEY_ID}" ]] && [[ ! -z "${AWS_SECRET_ACCESS_KEY}" ]]; then
+cat <<EOF > ~/.aws/credentials
 [default]
 aws_access_key_id = $AWS_ACCESS_KEY_ID
 aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
 EOF
+fi
 
 /usr/local/bin/goofys -f --endpoint $S3_URL $OPTION $S3_BUCKET $MNT_POINT
